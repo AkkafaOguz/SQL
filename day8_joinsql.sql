28d1905abc6c22d830b2b3daec50bf167d3681c1
Oguzhan <akkafaoguz@gmail.com>
2021-09-11 21:18:47 +0300

    day8

--- day8_joinsql.sql
+++ day8_joinsql.sql
@@ -27,7 +27,8 @@ CREATE TABLE sirketler
     INSERT INTO sirketler VALUES(101, 'Honda');
     INSERT INTO sirketler VALUES(102, 'Ford');
     INSERT INTO sirketler VALUES(103, 'Hyundai');
-    CREATE TABLE siparisler
+    
+   CREATE TABLE siparisler
     (
         siparis_id NUMBER(9),
         sirket_id NUMBER(9),
@@ -59,10 +60,138 @@ CREATE TABLE sirketler
 ------------------------------------------------------------------------------*/
    
    
-   SELECT sirket_isim, siparis_id, siparis_tarihi FROM SIRKETLER s FULL JOIN siparisler sp ON s.SIRKET_ID= sp.SIRKET_ID; 
+   SELECT sirket_isim, siparis_id, siparis_tarihi, sp.SIRKET_ID, s.SIRKET_ID FROM SIRKETLER s FULL JOIN siparisler sp ON s.SIRKET_ID= sp.SIRKET_ID; 
   
 --ORNEK2: Iki Tabloda sirket_id’si ayni olanlarin sirket_ismi, siparis_id ve
 --siparis_tarihleri listeleyen bir sorgu yaziniz. null olanlar çýkmýcak çünkü NULL ortak deðil(bir tabloda null iken diðerinde id null yok, 100, 101 vs var)
    
+  
+/*=============================== INNER JOIN  ==================================
+    Iki tablonun kesisim kumesi ile yeni bir tablo olusturmak icin kullanilir.
+    Syntax
+    -----------
+    SELECT sutun1,sutun2....sutunN
+    FROM tablo1
+    INNER JOIN tablo2
+    ON tablo1.sutun = tablo2.sutun;
+================================================================================
+
+
+/* -----------------------------------------------------------------------------
+  ORNEK2: Iki Tabloda sirket_id’si ayni olanlarin sirket_ismi, siparis_id ve
+  siparis_tarihleri listeleyen bir sorgu yaziniz. null olanlar çýkmýcak çünkü NULL
+  ortak deðil(bir tabloda null iken diðerinde id null yok, 100, 101 vs var)
+------------------------------------------------------------------------------*/
+  
+
+   SELECT sirket_isim, siparis_id, siparis_tarihi, sp.sirket_id FROM SIRKETLER s INNER JOIN SIPARISLER sp ON s.SIRKET_ID = sp.SIRKET_ID ;
+  
+-- INNER JOIN ile sadece iki tablodaki ortak olan satirlar secilir.
+-- Diger ifadeyle iki tablodaki ortak olan sirket_id degerleri icin ilgili sutunlar listenir.
+  
+  
+  
+  
+  
+  
+/*=============================== LEFT JOIN  ==================================
+    LEFT JOIN, 1. tablodan (sol tablo) SELECT ile ifade edilen sutunlara ait tum
+    satirlari getirir.
+    Ancak, diger tablodan sadece ON ile belirtilen 2.tablodan kosula uyan satirlari getirir.
+    -- ancak ortak olmayan kisimlar bos kalir. 2. tablodan 104,105 yok mesela.
+    Syntax
+    -----------
+    SELECT sutun1,sutun2....sutunN
+    FROM tablo1
+    LEFT JOIN tablo2
+    ON tablo1.sutun = tablo2.sutun;
+==============================================================================*/
+  
+  
+  /* -----------------------------------------------------------------------------
+  ORNEK3: sirketler tablosundaki tum sirketleri ve bu sirketlere ait olan
+  siparis_id ve siparis_tarihleri listeleyen bir sorgu yaziniz.
+------------------------------------------------------------------------------*/
+  
+  SELECT sirket_isim, siparis_id, siparis_tarihi FROM SIRKETLER s LEFT JOIN SIPARISLER sp ON s.SIRKET_ID = sp.SIRKET_ID ;
+ 
+--From sonrasi patron tablo (for LEFT JOIN) JOIN sonrasi cocuk tablo (for RIGHT JOIN)  
+ 
+  
+ 
+/*============================== RIGHT JOIN  ==================================
+    RIGHT JOIN, 2. tablodan (sag tablo) SELECT ile ifade edilen sutunlara ait tum
+    satirlari getirir.
+     diger tablodan sadece ON ile belirtilen 1. tablodan kosula uyan (ortak) satirlari getirir.
+    ancak ortak olmayan kisimlar bos kalir.1. tablodaki Toyota yazýlmaz, ortak olmadýðý için
+    Syntax
+    -----------
+    SELECT sutun1,sutun2....sutunN
+    FROM tablo1
+    RIGHT JOIN tablo2
+    ON tablo1.sutun = tablo2.sutun;
+==============================================================================*/
+
+ 
+ /* -----------------------------------------------------------------------------
+  ORNEK4: siparisler tablosundaki tum siparis_id ve siparis_tarihleri ile
+  bunlara karþýlýk gelen sirket_isimlerini listeleyen bir sorgu yaziniz.
+------------------------------------------------------------------------------*/
+  
+ SELECT siparis_id, siparis_tarihi,sirket_isim FROM SIRKETLER s  RIGHT JOIN SIPARISLER sp ON s.SIRKET_ID = sp.SIRKET_ID ;
+  
+
+
+
+CREATE TABLE bolumler (
+      bolum_id   NUMBER(2) ,
+      bolum_isim VARCHAR2(14),
+      konum      VARCHAR2(13)
+    );
+  
+   INSERT INTO bolumler VALUES (10,'MUHASABE','IST');
+    INSERT INTO bolumler VALUES (20,'MUDURLUK','ANKARA');
+    INSERT INTO bolumler VALUES (30,'SATIS','IZMIR');
+    INSERT INTO bolumler VALUES (40,'ISLETME','BURSA');
+    INSERT INTO bolumler VALUES (50,'DEPO', 'YOZGAT');
+    CREATE TABLE personel (
+      personel_id   NUMBER(4) ,
+      personel_isim VARCHAR2(10),
+      meslek        VARCHAR2(9),
+      mudur_id      NUMBER(4),
+      ise_baslama   DATE,
+      maas          NUMBER(7,2),
+      bolum_id      NUMBER(2)
+    );
+   SELECT * FROM bolumler;
+   
+  INSERT INTO personel VALUES (7369,'AHMET','KATIP',7902,'17.12.1980',800,20);
+    INSERT INTO personel VALUES (7499,'BAHATTIN','SATIS',7698,'20.02.1981',1600,30);
+    INSERT INTO personel VALUES (7521,'NESE','SATIS',7698,'22.02.1981',1250,30);
+    INSERT INTO personel VALUES (7566,'MUZAFFER','MUDUR',7839,'02.04.1981',2975,20);
+    INSERT INTO personel VALUES (7654,'MUHAMMET','SATIS',7698,'28.09.1981',1250,30);
+    INSERT INTO personel VALUES (7698,'EMINE','MUDUR',7839,'01.05.1981',2850,30);
+    INSERT INTO personel VALUES (7782,'HARUN','MUDUR',7839,'09.06.1981', 2450,10);
+    INSERT INTO personel VALUES (7788,'MESUT','ANALIST',7566,'13.07.87',3000,20);
+    INSERT INTO personel VALUES (7839,'SEHER','BASKAN',NULL,'17.11.1981',5000,10);
+    INSERT INTO personel VALUES (7844,'DUYGU','SATIS',7698,'08.09.1981',1500,30);
+    INSERT INTO personel VALUES (7876,'ALI','KATIP',7788,'13.07.1987',1100,20);
+    INSERT INTO personel VALUES (7900,'MERVE','KATIP',7698,'03.12.1981',950,30);
+    INSERT INTO personel VALUES (7902,'NAZLI','ANALIST',7566,'03.12.1981',3000,20);
+    INSERT INTO personel VALUES (7934,'EBRU','KATIP',7782,'23.01.1982',1300,10);
+    INSERT INTO personel VALUES (7956,'SIBEL','MIMAR',7782,'29.01.1991',3300,60);
+    INSERT INTO personel VALUES (7933,'ZEKI','MUHENDIS',7782,'26.01.1987',4300,60);
+    SELECT * FROM personel;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
